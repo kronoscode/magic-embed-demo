2a6690d5d045c5a42970975c6b4c89d2fdbd4749
Cajetero <om.cortez.2010@gmail.com>
2014-09-05 14:49:29 -0600

    solved bad url

--- med/config/urls.py
+++ med/config/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     # url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('magic.urls', namespace="magic")),
+    url(r'', include('magic.urls', namespace="magic")),
 )
